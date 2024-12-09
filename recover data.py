3d303c7fb4d6659d6d2954c8834331f6603a724f
MohsenAK <152519616+akoumavich@users.noreply.github.com>
2024-12-09 15:25:02 +0100

    Update recover data.py

--- recover data.py
+++ recover data.py
@@ -1,8 +1,10 @@
 import numpy as np
-f=open("facebook_combined.txt",'r')
-nb_nodes=4039
-A=np.zeros((nb_nodes,nb_nodes),dtype='int')
-for line in f:
-    [i,j]=line.split(' ')
-    i,j=int(i),int(j)
-    A[i,j]=A[j,i]=1
+def recover_data(path="facebook_combined.txt"):
+    f=open(path,'r')
+    nb_nodes=4039
+    A=np.zeros((nb_nodes,nb_nodes),dtype='int')
+    for line in f:
+        [i,j]=line.split(' ')
+        i,j=int(i),int(j)
+        A[i,j]=A[j,i]=1
+    return A
